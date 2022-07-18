fc7c10b574a1b8b7b90133f76e9a7ad8eebb3749
waqaskhan <waqas.khan@optimusfox.com>
2022-07-18 09:06:30 +0500

    Axios interpretor issue solved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CartPage from "./pages/cart/CartPage";
-import Home from "./pages/home/Home";
-import Item from "./pages/item/Item";
-import Login from "./pages/login/Login";
-import Registration from "./pages/registration/Registration";
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CartPage from './pages/cart/CartPage';
+import Home from './pages/home/Home';
+import Item from './pages/item/Item';
+import Login from './pages/login/Login';
+import Registration from './pages/registration/Registration';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/items/:id" element={<Item />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Registration />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/items/:id' element={<Item />} />
+          <Route path='/cart' element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster position='top-center' reverseOrder={false} />
+    </>
   );
 }
 
